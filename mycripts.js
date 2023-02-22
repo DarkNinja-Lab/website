0e5c3422bd597640265fd748620f74c792876b76
Dominic Hahn <116317412+DarkNinja-Lab@users.noreply.github.com>
2023-02-22 17:05:52 +0100

    Add files via upload

---
+++ mycripts.js
@@ -0,0 +1,32 @@
+export const urlsArray = [
+    //
+    "path/to/game1.zip",
+    "http://192.168.0.6:8384/Metro%20Exodus/Metro_Exodus.iso",
+    "http://192.168.0.6:8384/RimWorld.rar",
+    "http://192.168.0.6:8384/Goat.Simulator.3-P2P.zip",
+    "http://192.168.0.6:8384/Drift%20Racing%20Online.rar",
+    "http://192.168.0.6:8384/Underrrreerrgro2.7z",
+    "http://192.168.0.6:8384/Drift%20Racing%20Online.rar",
+    "http://192.168.0.6:8384/Need.for.Speed.Carbon.MULTi12.rar",
+    "http://192.168.0.6:8384/Melatonin.rar",
+    "http://192.168.0.6:8384/WRC_Generations_The_FIA_WRC_Official_Game-FLT/wrc_generations.iso",
+    "http://192.168.0.6:8384/rzr-settlers/rzr-settlers.iso",
+    "http://192.168.0.6:8384/Need.for.Speed.Carbon.MULTi12.rar",
+    "http://192.168.0.6:8384/NeedForSpeedMostWantedBlackEdition.zip",
+    "http://192.168.0.6:8384/God_of_War-FLT/god_of_war.iso",
+    "http://192.168.0.6:8384/BeamNG.rar",
+    "path/to/game16.zip",
+    "http://192.168.0.6:8384/a%20VR%20GAMING/Half.Life.Alyx.zip",
+    "path/to/game18.zip",
+    "path/to/game19.zip",
+    "path/to/game20.zip",
+    "http://192.168.0.6:8384/South.Park.The.Stick.of.Truth.rar",
+    "http://192.168.0.6:8384/a%20VR%20GAMING/SUPERHOT%20VR.rar",
+    "http://192.168.0.6:8384/Yomi_Hustle.zip",
+    "http://192.168.0.6:8384/BONELAB.zip",
+    "path/to/game25.zip", // Boneworks
+    "http://192.168.0.6:8384/Rogue.AI.Simulator.v1.0.2.rar",
+    "http://192.168.0.6:8384/Brewmaster%20BBS%20%5bFitGirl%20Repack%5d.rar",
+// ... 
+];
+
